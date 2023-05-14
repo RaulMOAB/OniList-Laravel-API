4f96947e780eb4bb6db13c575bc19c8fe1de5851
AMGG13 <alvinmillergarciagarcia@gmail.com>
2023-05-14 21:55:06 +0200

    cors

--- app/Http/Middleware/OniCors.php
+++ app/Http/Middleware/OniCors.php
@@ -17,12 +17,12 @@ class OniCors
     {
         $allowedOrigins = ['http://localhost:3000', 'https://onilist.club'];
         $origin = $_SERVER['HTTP_ORIGIN'];
-        if(in_array($origin,$allowedOrigins)){
+
             $response = $next($request);
-            $response->header('Access-Control-Allow-Origin', $origin);
+            $response->header('Access-Control-Allow-Origin', "*");
             $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
             $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        }
+
 
         return $response;
     }
